5e5a75ae4e2397d8a734ee422f197ea529fa1bdb
jeroenheijmans <github@jeroenheijmans.nl>
2016-09-21 22:56:53 +0200

    Removes setters from ICheckResult properties
    Fixes #12

--- src/Nimator/ICheckResult.cs
+++ src/Nimator/ICheckResult.cs
@@ -9,12 +9,12 @@
         /// Repeats the name of the <see cref="ICheck"/>, possibly in a format altered to
         /// work best for display purposes.
         /// </summary>
-        string CheckName { get; set; }
+        string CheckName { get; }
 
         /// <summary>
         /// The <see cref="NotificationLevel"/> for this result (e.g. "Okay", or "Error").
         /// </summary>
-        NotificationLevel Level { get; set; }
+        NotificationLevel Level { get; }
 
         /// <summary>
         /// Joins <see cref="Level"/>, <see cref="CheckName"/>, and any other details, in 
